b221e330886ed22238663221d0cb0591577f5647
leia <66150841+leiapei@users.noreply.github.com>
2024-06-22 08:07:04 -0700

    Update montecarlo2_sh.sh

--- preprocessing/montecarlo2_sh.sh
+++ preprocessing/montecarlo2_sh.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-directory='MonteCarlo1'
+directory='MonteCarlo1' # directory where previous files are stored
 
+# executed if file does not exist
 if [ ! -d "$directory" ]; then
     echo "not found"
     exit 1
 fi
 
+# executed if file exists
 for file in "$directory"/*; do
   if [ -f "$file" ]; then
     echo "$file"
@@ -22,7 +24,7 @@ for file in "$directory"/*; do
         name=${name%.*}
         echo ${name}
         echo "double${name}.xyz"
-        obconformer 250 100 "$file" > "mc${name}.xyz"
+        obconformer 250 100 "$file" > "mc${name}.xyz" # stores newly conformed files in new xyz file
     fi
     ((i+=1))
 done
