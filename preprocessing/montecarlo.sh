c2a918beced9ac3b7319d0c7a95f13f4ffa45566
leia <66150841+leiapei@users.noreply.github.com>
2024-06-22 07:10:13 -0700

    Update montecarlo.sh

--- preprocessing/montecarlo.sh
+++ preprocessing/montecarlo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-directory='630outfiles'
+directory='630outfiles' # directory where all SMILES files were stored
 
+# executes if directory doesn't exist
 if [ ! -d "$directory" ]; then
     echo "not found"
     exit 1
 fi
 
+# iterates through each file in 630_outfiles
 for file in "$directory"/*; do
   if [ -f "$file" ]; then
     echo "$file"
@@ -22,7 +24,7 @@ for file in "$directory"/*; do
         name=${name%.*}
         echo ${name}
         echo "montecarlo${name}.xyz"
-        obconformer 250 100 "$file" > "montecarlo${name}.xyz"
+        obconformer 250 100 "$file" > "montecarlo${name}.xyz" # calling obconformer & placing file into corresponding montecarlo__.xyz file
     fi
     ((i+=1))
 done
