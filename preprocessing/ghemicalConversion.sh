70afbaaebbeb3936ffdd8aeed404b2220e39827f
leia <66150841+leiapei@users.noreply.github.com>
2024-06-21 21:28:11 -0700

    rename + annotated ghemicalConversion.sh

--- preprocessing/ghemicalConversion.sh
+++ preprocessing/ghemicalConversion.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+:'
+1. iterates through every single structure stored in all_structures
+2. converts each .xyz file to .gpr (Ghemical format)
+'
+
 directory='all_structures'
 
 if [ ! -d "$directory" ]; then
@@ -22,6 +27,7 @@ for file in "$directory"/*; do
         name=${name%.*}
         echo ${name}
         echo "${name}.gpr"
+        # using OpenBabel to convert xyz files to Ghemical format and stores it in its .gpr equivalent
         obabel -ixyz  "$file" -ogpr -O "${name}.gpr"
     fi
     ((i+=1))
