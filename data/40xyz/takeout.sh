dc2fad7c5ccb22acea3cc5cacfeed141b02f6ed4
emmaW812 <150621031+emmaW812@users.noreply.github.com>
2024-09-23 02:18:27 -0700

    Delete data/40xyz directory

--- data/40xyz/takeout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Directory to clean (defaults to the current directory if not provided)
-DIRECTORY='/Users/leiapei/Downloads/2dshifts/outfiles'
-
-# Remove all .out files within the directory
-find "$DIRECTORY" -type f -name "*.out" -exec rm -f {} \;
-
-echo "All .out files in $DIRECTORY have been removed."
